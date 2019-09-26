6a58ff5d435e1beefc12093aebd757315201b525
irbris <55032489+irbris@users.noreply.github.com>
2019-09-26 20:04:39 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 module.exports = function zeros(expression) {
-  // your solution
+  const arr = expression.split('*');
+  const arrSimpleFactorial = [];
+  const arrDoubleFactorial = [];
+  let numberOfZero = 0;
+  let even = 0;
+  
+  arr.map(element => {
+    if(element.endsWith('!!')) {
+      arrDoubleFactorial.push(element.slice(0, -2));
+    } else {
+      arrSimpleFactorial.push(element.slice(0, -1));
+    }
+  })
+  
+  arrDoubleFactorial.map(number => {
+    if (number % 2 === 0) {
+        even += 1;
+      }
+      for (let i = number; i > 0; i -= 2) {
+        if (i % 5 === 0) {
+          numberOfZero += 1;
+          if (i % 25 === 0) {
+            numberOfZero += 1;
+          }
+        }
+      }
+  })
+  
+  arrSimpleFactorial.map(number => {
+    numberOfZero += Math.floor(number / 5) + Math.floor(number / 25);
+    even += 1;
+  })      
+
+  
+  if(!even) return 0;
+  return numberOfZero;
 }
